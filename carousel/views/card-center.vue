3205cea0259b8fdc9259ea7bab87129e570151b3
guruprasaad123 <jiiva116@live.com>
2017-11-21 23:59:40 +0500

    contact yet to finish

---
+++ carousel/views/card-center.vue
@@ -0,0 +1,44 @@
+<template>
+
+ <div class="card text-center" style="width: 20rem;margin-right:150px">
+              <div class="card-body">
+                <h4 class="card-title" >{{title}}</h4>
+                <p class="card-text">
+              <slot>
+              </slot>   
+                </p>
+                <a href="#" class="btn btn-primary">Go somewhere</a>
+              </div>
+            </div>
+
+</template>
+
+<script>
+
+module.exports = {
+    props: {
+        title: {required: true},
+       styling:{default:''}
+    },
+    methods:{
+        anchor(head)
+        {
+return `<a href="#" >${head}</a>`
+        }
+
+    },
+    data(){
+      return{
+
+      };
+    }
+
+}
+
+</script>
+<style scoped>
+span{
+      color:gray;
+  font-weight:bolder;
+}
+</style>
